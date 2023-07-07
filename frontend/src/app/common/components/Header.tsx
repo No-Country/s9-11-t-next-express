81b194a15db46f22de7576fdfcc17938cb60c156
manuelperezdearce <manuel.perezdearce@gmail.com>
2023-07-06 20:34:08 -0400

    F/manuel:initial layout

---
+++ frontend/src/app/common/components/Header.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+
+export default function Header() {
+    return (
+        <header className="bg-yellow-300">
+            <Navbar />
+        </header>
+
+    )
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <main >
-      <h1>Clon de Meli</h1>
+    <main className="min-h-[78vh] bg-gray-200">
+      <div className="container">
+        <div className="px-2 py-3">
+          <h1>Home</h1>
+        </div>
+      </div>
     </main>
   )
 }

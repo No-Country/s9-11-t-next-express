abab316ae38961a74134a8b7be87ff17bfed18ce
fabian jorge sanchez <sjorgefabi@gmail.com>
2023-07-13 00:57:41 -0300

    refactor(users): service; cloudinary config. fix(users): dto - optional fields

--- backend/src/cloudinary/cloudinary.provider.ts
+++
@@ -1,8 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary'
-import { CLOUDINARY, CLOUDINARY_CONFIG } from './constants'
-export const CloudinaryProvider = {
-  provide: CLOUDINARY,
-  useFactory: () => {
-    return cloudinary.config(CLOUDINARY_CONFIG)
-  },
-}
--- backend/src/cloudinary/cloudinary.service.ts
+++ backend/src/cloudinary/cloudinary.service.ts
@@ -4,20 +4,31 @@ import {
   UploadApiResponse,
   v2 as cloudinary,
 } from 'cloudinary'
-import { CLOUDINARY_FOLDER } from './constants'
+import { CLOUDINARY_CONFIG, CLOUDINARY_FOLDERS } from './constants'
+import { ConfigService } from '@nestjs/config'
+
 @Injectable()
 export class CloudinaryService {
+  constructor(private configService: ConfigService) {
+    cloudinary.config({
+      cloud_name: this.configService.get<string>(CLOUDINARY_CONFIG.NAME),
+      api_key: this.configService.get<string>(CLOUDINARY_CONFIG.KEY),
+      api_secret: this.configService.get<string>(CLOUDINARY_CONFIG.SECRET),
+      secure: true,
+    })
+  }
   async uploadFileCloudinary(
     file: Express.Multer.File,
     publicId: string,
+    folder: string = CLOUDINARY_FOLDERS.DEFAULT,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return new Promise((resolve, reject) => {
       cloudinary.uploader
         .upload_stream(
           {
             resource_type: 'auto',
-            folder: CLOUDINARY_FOLDER,
-            public_id: `${publicId}`,
+            folder,
+            public_id: publicId,
             overwrite: true,
           },
           (error, result) => {

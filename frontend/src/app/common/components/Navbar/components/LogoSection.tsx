c74ccb6abae2e3f06453bb4efe1eb18db5a3ac52
srueda9331@gmail.com <90795981+srueda9331@users.noreply.github.com>
2023-07-26 17:18:25 -0500

    An error was resolved in component LogoSection from Navbar

--- frontend/src/app/common/components/Navbar/components/LogoSection.tsx
+++ frontend/src/app/common/components/Navbar/components/LogoSection.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation"
 
@@ -10,7 +11,7 @@ export default function LogoSection() {
     return (
         <div className="flex items-center">
             <Link href="/">
-                <img className="h-[34px] w-auto" href={'/page'} src={'https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.22.13/mercadolibre/logo__large_plus.png'} width={100} height={100} alt="logo" />
+                <Image className="h-[34px] w-auto" src={'https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.22.13/mercadolibre/logo__large_plus.png'} width={100} height={100} alt="logo" />
             </Link>
         </div>
     )
